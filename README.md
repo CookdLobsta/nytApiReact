304f078e3d36ef2eaf0f87cd20c8795d3790159d
Connor Wilson <connormwilson@gmail.com>
2019-01-24 23:18:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# New York Times React App
+
+
+Allows users to search the New York Times for articles and save articles for later review.
+
+## How to Use
+
+Enter a search topic along with start and end dates. The top 10 search results will be returned.
---
+++ client/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => (
+  <div>
+    <nav>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={
+              (window.location.pathname === "/" || window.location.pathname === "/home") ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/saved"
+            className={
+              window.location.pathname === "/saved" ? "nav-link active" : "nav-link"
+            }
+          >
+            Saved
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <header className="App-header jumbotron-fluid">
+      <h1 className="App-title">New York Times</h1>
+    </header>    
+  </div>
+);
+
+export default Header;
